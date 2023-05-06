f4dc99930c4bf7524844761285fdfc469f023043
Jakob Muehlberger <jakob@diemuehlbergers.at>
2023-05-06 16:24:06 +0200

    many cars + next slogan

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-const canvas = document.querySelector('#bg');
+const canvas = document.querySelector('#interactable1');
 
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
@@ -8,37 +8,28 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 // import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three-orbitcontrols@2.110.3/OrbitControls.min.js';
 // import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three-gltf-extensions@0.0.15/+esm';
 
-var scene, camera, renderer, cube;
+var scene, camera, renderer;
 
 scene = new THREE.Scene();
 camera = new THREE.PerspectiveCamera(50, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
 
 // Renderer
 renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
+  canvas: document.querySelector('#interactable1'),
 });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(canvas.clientWidth, canvas.clientHeight );
 
+// Set background transparent
+renderer.setClearAlpha(0);
 
-// canvas.width = 900;
-
-// scene.background = new THREE.Color(0x000000);
-
-renderer.setClearAlpha(0.0);
-
-// document.body.appendChild( renderer.domElement );
-
-// Geometry
-const geometry = new THREE.BoxGeometry(3.8, 3.8, 3.8);
-// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const material = new THREE.MeshNormalMaterial({ color: 0x00ff00 });
-
+// Set the Lights
 const ambientLight = new THREE.AmbientLight(0xFFFFFF, 1);
 scene.add(ambientLight);
 
 const light = new THREE.HemisphereLight(0xFFFFFF, 0xFFFFFF, 1);
 scene.add(light);
+
 const pointLight = new THREE.PointLight(0xFFFFFF, 5, 100);
 pointLight.position.set(5, 5, 5);
 scene.add(pointLight);
@@ -57,7 +48,11 @@ loader.load('./3d/car.gltf', function(gltf){
 
 
 //Cube
-// cube = new THREE.Mesh(geometry, material);
+
+// const geometry = new THREE.BoxGeometry(3.8, 3.8, 3.8);
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const material = new THREE.MeshNormalMaterial();
+// var cube = new THREE.Mesh(geometry, material);
 
 // cube.position.x = 0;
 // cube.position.y = 0;
@@ -65,17 +60,14 @@ loader.load('./3d/car.gltf', function(gltf){
 
 // scene.add(cube);
 
-// Camera
+// Set Camera Position
 camera.position.z = 3;
 camera.position.y = 2.5;
 camera.position.x = 3;
-
-// camera.lookAt(car.position);
+camera.enabledampening = true;
 
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
-
-
 
 // Animation
 function animate() {
@@ -88,3 +80,68 @@ function animate() {
 }
 
 animate();
+
+
+/* Second frame */
+// Delete when reactified
+
+const canvas2 = document.querySelector('#interactable2');
+
+var scene2, camera2, renderer2;
+
+scene2 = new THREE.Scene();
+camera2 = new THREE.PerspectiveCamera(50, canvas2.clientWidth / canvas2.clientHeight, 0.1, 1000);
+
+// Renderer
+renderer2 = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#interactable2'),
+});
+renderer2.setPixelRatio(window.devicePixelRatio);
+renderer2.setSize(canvas.clientWidth, canvas.clientHeight );
+
+// Set background transparent
+renderer2.setClearAlpha(0);
+
+// Set the Lights
+const ambientLight2 = new THREE.AmbientLight(0xFFFFFF, 1);
+scene2.add(ambientLight2);
+
+const light2 = new THREE.HemisphereLight(0xFFFFFF, 0xFFFFFF, 1);
+scene2.add(light2);
+
+const pointLight2 = new THREE.PointLight(0xFFFFFF, 5, 100);
+pointLight2.position.set(5, 5, 5);
+scene2.add(pointLight2);
+
+
+// Car + GLTF Loader
+var loader2 = new GLTFLoader();
+
+var car2;
+
+loader2.load('./3d/car.gltf', function(gltf){
+  car2 = gltf.scene;
+  car2.position.setY(-0.7);
+  scene2.add(car2);
+});
+
+// Set Camera Position
+camera2.position.z = 3;
+camera2.position.y = 2.5;
+camera2.position.x = 3;
+camera2.enabledampening = true;
+
+// Controls
+const controls2 = new OrbitControls(camera2, renderer2.domElement);
+
+// Animation
+function animate2() {
+    requestAnimationFrame(animate2);
+
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
+
+    renderer2.render(scene2, camera2);
+}
+
+animate2();
---
+++ slider.js
@@ -0,0 +1,189 @@
+/* Slider Logic for the fancy Slider */
+// @Author: Jakob Mühlberger
+
+// Get the slider container and the buttons
+var container = document.querySelector(".slider");
+var prev = document.querySelector(".prev-slide-btn");
+var next = document.querySelector(".next-slide-btn");
+
+// Execute the moveCursor function on mousemove event on the window and the buttons
+window.onmousemove = (event) => moveCursor(event);
+prev.onmousemove = (event) => moveCursor(event);
+next.onmousemove = (event) => moveCursor(event);
+
+// Check the cursor position and move the buttons
+const moveCursor = (event) => {
+    let windowWidth = window.innerWidth;
+
+    var x = event.clientX;
+    var y = event.clientY;
+
+    // Check which button should be active
+    if (windowWidth / 2 < x) {
+        next.style.top = `${y}px`;
+        next.style.left = `${x}px`;
+        next.classList.add("active");
+        prev.classList.remove("active");
+    } else {
+        prev.style.top = `${y}px`;
+        prev.style.left = `${x}px`;
+        prev.classList.add("active");
+        next.classList.remove("active");
+    }
+
+    // Get the slider position and height
+
+    let windowScrollTop = window.pageYOffset;
+    let sliderOffset = container.offsetTop;
+    let sliderHeight = container.offsetHeight;
+
+    let relativeTop = sliderOffset - windowScrollTop;
+
+    let relativeBottom = relativeTop + sliderHeight;
+
+
+    // Check if cursor is in slider
+    if (y > relativeTop && y < relativeBottom) {
+        // checkIfOverButton(x, y, windowScrollTop)
+    } else {
+        prev.style.top = 0;
+        next.style.top = 0;
+        prev.classList.remove("active");
+        next.classList.remove("active");
+    }
+
+};
+
+
+
+let index = 0;
+const slides = container.querySelectorAll(".slide");
+
+const breakWidth = 100 * 16;
+const mobileBreakWidth = 40 * 16;
+const tabletBreakWidth = 60 * 16;
+
+let offset = breakWidth * 0.1;
+let sideMargin = 0;
+
+let slideOffset = 0;
+
+window.onresize = () => handleResize();
+
+// set margin of first slide
+const handleResize = () => {
+    if (window.innerWidth > breakWidth) {
+        sideMargin = (window.innerWidth - breakWidth) / 2;
+    } else {
+        sideMargin = 16;
+        offset = (window.innerWidth - 32) * 0.1;
+    }
+
+    if (window.innerWidth > mobileBreakWidth) {
+        slides[0].style.marginLeft = `${offset + sideMargin}px`;
+    }
+
+    // Make all slides active on mobile and disable buttons
+    if (window.innerWidth <= tabletBreakWidth) {
+        slides.forEach((slide) => {
+        slide.classList.add("active-slide");
+        // console.log("active-slide");
+        });
+        next.style.display = "none";
+        prev.style.display = "none";
+    } else {
+        slides.forEach((slide) => slide.classList.remove("active-slide"));
+        slides[index].classList.add("active-slide");
+        next.style.display = "block";
+        prev.style.display = "block";
+    }
+
+    updateSlider();
+};
+
+handleResize();
+
+prev.addEventListener("click", () => decreaseIndex());
+next.addEventListener("click", () => increaseIndex());
+
+// onclick function of buttons
+function increaseIndex() {
+    if (index < slides.length - 1) {
+        index++;
+        updateSlider();
+    }
+}
+
+function decreaseIndex() {
+    if (index > 0) {
+        index--;
+        updateSlider();
+    }
+}
+
+// update slider
+function updateSlider() {
+    slides.forEach((slide) => slide.classList.remove("active-slide"));
+    slides[index].classList.add("active-slide");
+    console.log("Index: " + index);
+
+    slideOffset = index * (640 + 32);
+
+    slides.forEach(
+        (slide) => (slide.style.transform = `translateX(${-slideOffset}px)`)
+    );
+    // console.log(offset + sideMargin - slideOffset);
+}
+
+// const checkIfOverButton = (x, y, windowScrollTop) => {
+
+
+//     let activeButton = slides[index].querySelector(".btn");
+    
+//     if (activeButton){
+//         // console.log("activeButton");
+
+//         var absoluteTop = cumulativeOffset(activeButton).top;
+//         let buttonOffset = absoluteTop - windowScrollTop;
+
+//         // let buttonOffset = umulativeOffset(activeButton).;
+//         console.log(buttonOffset);
+//         let buttonLeft = cumulativeOffset(activeButton).left;
+//         console.log(buttonLeft);
+
+//         // let buttonOffset = activeButton.offsetTop;
+//         let buttonHeight = activeButton.offsetHeight;
+//         // let buttonLeft = activeButton.;
+//         let buttonWidth = activeButton.offsetWidth;
+
+//         let relativeTop = buttonOffset - windowScrollTop;
+
+//         let relativeBottom = relativeTop + buttonHeight;
+
+//         let relativeLeft = buttonLeft;
+//         let relativeRight = relativeLeft + buttonWidth;
+
+//         if ((y > relativeTop) && y < relativeBottom && x > relativeLeft && x < relativeRight) {
+//             console.log("overButton");
+//             // prev.style.top = 0;
+//             // next.style.top = 0;
+//             // prev.classList.remove("active");
+//             // next.classList.remove("active");
+//         }
+
+//     }
+// }
+
+// var cumulativeOffset = function(element) {
+//     var top = 0, left = 0;
+//     do {
+//         top += element.offsetTop  || 0;
+//         left += element.offsetLeft || 0;
+//         element = element.offsetParent;
+//     } while(element);
+
+//     return {
+//         top: top,
+//         left: left
+//     };
+// };
